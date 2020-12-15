f22fbd4863d611501420aed5f9cb3b1f77c98334
lusalaMark <lusalamark63@gmail.com>
2020-12-15 12:19:48 +0300

    iterating overy elements in an array

---
+++ loops/index.php
@@ -0,0 +1,16 @@
+<?php
+//$names = ['mark', 'lusala', 'sigu', 'achiever'];
+//echo $names[0];
+$arr = array(
+    'CEO'  => 'Mark',
+    'Manager'  => 'Mortrk',
+    'Director'  => 'Marey',
+);
+
+foreach ($arr as $title => $name){
+    echo "<li><strong>$title</strong> - $name</li>";
+}
+
+
+
+?>  
