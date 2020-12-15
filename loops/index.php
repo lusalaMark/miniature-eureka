f7895b80fc07c96f1440a14deaaf78e692336de0
lusalaMark <lusalamark63@gmail.com>
2020-12-15 13:27:05 +0300

    Loops in php

--- loops/index.php
+++ loops/index.php
@@ -1,16 +1,20 @@
 <?php
+$arri = array('Mark','Lusala','Wim','Mudoga');
 //$names = ['mark', 'lusala', 'sigu', 'achiever'];
 //echo $names[0];
-$arr = array(
-    'CEO'  => 'Mark',
-    'Manager'  => 'Mortrk',
-    'Director'  => 'Marey',
-);
+//$arr = array(
+//  'CEO'  => 'Mark',
+//  'Manager'  => 'Mortrk',
+//   'Director'  => 'Marey',
+//);
 
-foreach ($arr as $title => $name){
-    echo "<li><strong>$title</strong> - $name</li>";
+//foreach ($arr as $title => $name){
+//    echo "<li><strong>$title</strong> - $name</li>";
+//}
+
+$i = 0;
+while ($i  < 10){
+    echo "<li>$arri[$i]</li>";
 }
-
-
 
 ?>  
