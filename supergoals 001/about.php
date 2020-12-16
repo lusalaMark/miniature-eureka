858106c626be7b67e09244bd3aefe73d9d5d51ea
lusalaMark <lusalamark63@gmail.com>
2020-12-16 16:42:02 +0300

    passing values from page to page

---
+++ supergoals 001/about.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About page</title>
+</head>
+<body>
+    <h1>Super Goals</h1>
+    <?php echo htmlspecialchars($_GET['name']);?>
+</body>
+</html>
---
+++ supergoals 001/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Super Goals</title>
+</head>
+<body>
+    <h1>Supper Goals</h1>
+    <?php
+/*     if (isset($_GET['id'])){
+        echo $_GET['id'];
+    }else{
+        echo "Redirecting somewhere ";
+    }
+     */
+
+    ?>
+    <a href="about.php?name=mark">About</a>
+</body>
+</html>
