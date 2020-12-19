a9c04f91c02ab3345d89eadbb34cb26ac2595147
lusalaMark <lusalamark63@gmail.com>
2020-12-19 04:07:30 +0300

    External page submission

--- demo/form.php
+++ demo/form.php
@@ -8,17 +8,17 @@ if(isset($_POST['submit'])){
     $name = array("Mark","Wim","Shammah","Set","Faith","Sharon");
     
     if(strlen($username) < $minimum ){
-        //echo "Username has to longer than 5 ";
+      echo "Username has to longer than 5 ";
     }
     
     if(strlen($username) > $maximum){
-      //  echo "Username has to be less than 10";
+       echo "Username has to be less than 10";
     }
     
-    //echo "Hello " . $username;
-    //echo "Your Password is " .$password;
+    echo "Hello " . $username;
+    echo "Your Password is " . $password;
     
-    if(!in_array($username, $name)){
+    if(in_array($username, $name)){
         echo "Sorry you cannot login";
     }else{
         echo 'Welcome';
@@ -34,7 +34,7 @@ if(isset($_POST['submit'])){
     <title>Checking form for data submission</title>
 </head>
 <body>
-    <form action="form.php" method="post">
+    <form action="form_process.php" method="post">
         <input type="text" name="username" placeholder="Enter Username">
         <br>
         <input type="password" name="password" placeholder="Enter Password">
